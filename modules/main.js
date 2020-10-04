ad061db2a6ffb819d3cd88da2c90eadbbfb842d5
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-10-04 08:47:46 +0200

    better keys buffer

--- modules/main.js
+++ modules/main.js
@@ -5,35 +5,21 @@ class Cross {
 
   #lastTime
   #counter
+  //#pressedKeys
+  #lastPressedKey
   #keysBuffer
-  #pressedKey
 
   constructor() {
     this.lastTime = window.performance.now()
     this.counter = 0
     this.keysBuffer = []
-    this.pressedKey = ['ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'][Math.floor(Math.random() * 4)]
+    this.lastPressedKey = ['ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'][Math.floor(Math.random() * 4)],
 
     window.addEventListener('keydown', (event) => {
       if (['ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'].includes(event.code)) {
         const index = this.keysBuffer.indexOf(event.code)
         if (index == -1) {
           this.keysBuffer.push(event.code)
-        }
-        this.pressedKey = event.code
-      }
-    })
-
-    window.addEventListener('keyup', (event) => {
-      if (['ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'].includes(event.code)) {
-        const index = this.keysBuffer.indexOf(event.code)
-        if (index != -1) {
-          this.keysBuffer.splice(index, 1)
-        }
-        if (this.keysBuffer.length == 0) {
-          this.pressedKey = event.code
-        } else {
-          this.pressedKey = this.keysBuffer[this.keysBuffer.length - 1]
         }
       }
     })
@@ -44,6 +30,51 @@ class Cross {
     const interval = 1000 / 1
 
     if (delta > interval) {
+      switch (this.lastPressedKey) {
+        case undefined: {
+          this.lastPressedKey = this.keysBuffer[this.keysBuffer.length - 1]
+          break
+        }
+
+        case 'ArrowUp': {
+          const keys = this.keysBuffer.filter((key) => key !== 'ArrowDown')
+          const last = keys[keys.length - 1]
+          if (last) {
+            this.lastPressedKey = last
+          }
+          break
+        }
+
+        case 'ArrowDown': {
+          const keys = this.keysBuffer.filter((key) => key !== 'ArrowUp')
+          const last = keys[keys.length - 1]
+          if (last) {
+            this.lastPressedKey = last
+          }
+          break
+        }
+
+        case 'ArrowLeft': {
+          const keys = this.keysBuffer.filter((key) => key !== 'ArrowRight')
+          const last = keys[keys.length - 1]
+          if (last) {
+            this.lastPressedKey = last
+          }
+          break
+        }
+
+        case 'ArrowRight': {
+          const keys = this.keysBuffer.filter((key) => key !== 'ArrowLeft')
+          const last = keys[keys.length - 1]
+          if (last) {
+            this.lastPressedKey = last
+          }
+          break
+        }
+
+      }
+
+      this.keysBuffer = []
       this.counter += Math.round(delta / interval)
       this.lastTime = currentTime
     }
@@ -72,7 +103,7 @@ class Cross {
     buffer.font = 'bold 40px Arial';
     buffer.fillStyle = "red"
 
-    const text = `[${this.counter} / [${this.keysBuffer.join('+')}] / ${this.pressedKey}]`
+    const text = `[${this.counter} / [${this.keysBuffer.join('+')}] / ${this.lastPressedKey}]`
 
     const textW = buffer.measureText(text).width
     buffer.fillRect((viewport.width / 2) - textW / 2, (viewport.height / 2) - 22, textW, 42)
@@ -85,5 +116,5 @@ const cross = new Cross()
 const canvas = new Canvas('canvas', [cross])
 
 window.addEventListener("load", function() {
-  new Loop(30, [canvas, cross]).run()
+  new Loop(60, [canvas, cross]).run()
 })
