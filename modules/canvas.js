9c2206aeaaf85d7ebef79f909c431423fc479c39
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-10-12 12:11:04 +0200

    offscreen rendering

--- modules/canvas.js
+++ modules/canvas.js
@@ -1,5 +1,7 @@
 import Rectangle from './rectangle.js'
 
+const PIXEL_SIZE = 1
+
 class Canvas {
 
   #screen
@@ -13,29 +15,33 @@ class Canvas {
   constructor(elemenId, children) {
     this.resizeEvent = null
     this.children = children
-    const buffer = document.createElement('canvas')
-    this.buffer = buffer.getContext('2d', { alpha: false, desynchronized: true })
     const ref = document.getElementById(elemenId)
-    this.screen = ref.getContext('2d', { alpha: false, desynchronized: false })
+    this.screen = ref.getContext('bitmaprenderer')
+    this.buffer = new OffscreenCanvas(this.screen.canvas.width, this.screen.canvas.height).getContext('2d', { alpha: false, desynchronized: true })
     this.pixelRatio = window.devicePixelRatio
     this.render = this.render.bind(this)
     this.onResize = this.onResize.bind(this)
-    this.viewport = new Rectangle()
+    this.viewport = new Rectangle(0, 0, this.buffer.canvas.width, this.buffer.canvas.height)
     window.addEventListener('resize', this.onResize)
     this.onResize()
   }
 
   onResize() {
     const wrapper = this.screen.canvas.parentElement
-    this.resizeEvent = {
-      width: wrapper.clientWidth * this.pixelRatio,
-      height: wrapper.clientHeight * this.pixelRatio,
+    const nextWidth = wrapper.clientWidth * this.pixelRatio
+    const nextHeight = wrapper.clientHeight * this.pixelRatio
+
+    if (nextWidth !== this.viewport.width || nextHeight !== this.viewport.height) {
+      this.resizeEvent = {
+        width: wrapper.clientWidth * this.pixelRatio,
+        height: wrapper.clientHeight * this.pixelRatio,
+      }
     }
   }
 
   render() {
     this.screen.imageSmoothingEnabled = false
-    this.screen.drawImage(this.buffer.canvas, 0, 0)
+    this.screen.transferFromImageBitmap(this.buffer.canvas.transferToImageBitmap())
   }
 
   update(currentTime) {
@@ -44,17 +50,18 @@ class Canvas {
     }
     this.updating = true
     if (this.resizeEvent) {
-      this.screen.canvas.width = this.buffer.canvas.width = this.resizeEvent.width
-      this.screen.canvas.height = this.buffer.canvas.height = this.resizeEvent.height
-      this.viewport.width = this.resizeEvent.width / this.pixelRatio
-      this.viewport.height = this.resizeEvent.height / this.pixelRatio
+      this.buffer.canvas.width = this.resizeEvent.width
+      this.buffer.canvas.height = this.resizeEvent.height
+      this.viewport.width = this.resizeEvent.width / this.pixelRatio / PIXEL_SIZE
+      this.viewport.height = this.resizeEvent.height / this.pixelRatio / PIXEL_SIZE
       this.resizeEvent = null
     }
-    this.buffer.setTransform(this.pixelRatio, 0, 0, this.pixelRatio, 0, 0)
+    this.buffer.imageSmoothingEnabled = false
+    this.buffer.setTransform(this.pixelRatio * PIXEL_SIZE, 0, 0, this.pixelRatio * PIXEL_SIZE, 0, 0)
     this.buffer.fillStyle = "white"
     this.buffer.fillRect(0, 0, this.viewport.width, this.viewport.height)
     this.children.forEach((child) => {
-      child.render(this.viewport, this.buffer)
+      child.render(this.viewport, this.buffer, currentTime)
     })
     this.updating = false
     this.render()
